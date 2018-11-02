ea14c50e1b11417befe7e4c3c1adb6c7143f0f8b
Santiago Ramírez Acevedo - Ceiba Software <santiago.ramirez@ceiba.com.co>
2018-11-02 13:06:30 -0500

    interfaz para el calculo del costo del parqueo

--- src/main/java/com/ceiba/estacionamiento/modelo/calculadoraCostoFacturaParqueo.java
+++
@@ -1,7 +0,0 @@
-package com.ceiba.estacionamiento.modelo;
-
-import com.ceiba.estacionamiento.persistencia.entity.FacturaEntity;
-
-public interface CalculadoraCostoFacturaParqueo {
-	public double calcularCosto(FacturaEntity factura);
-}
